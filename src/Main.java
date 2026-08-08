eaa4a8dce9e503b3607372d5cc4da9ff59ef4049
João Fernandes <jguiborges@gmail.com>
2026-08-07 21:12:13 -0300

    Sending first project

---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        Util util = new Util();
+
+        int tamanhoLista = 10000;
+        int limiteLista = 15000;
+        int[] lista = util.gerarLista(tamanhoLista, limiteLista);
+
+        boolean continuar = true;
+        int opcao = 0;
+        System.out.println("\n\nBem vindo!");
+
+        while(continuar){
+            System.out.println("\n\nDigite a opção desejada:");
+            System.out.println("0 - Sair");
+            System.out.println("1 - Ordenar lista");
+            System.out.println("2 - Listar numeros primos");
+            System.out.println("3 - Ordenar numeros primos");
+            System.out.println("4 - Gerar uma nova lista");
+            opcao = teclado.nextInt();
+
+            if(opcao < 0 || opcao > 3){
+                System.out.println("\n\nOpção inválida!\n\n");
+                continue;
+            }
+
+            switch (opcao){
+                // Sair
+                case 0:
+                    System.out.println("\n\nEncerrando programa!\n\n");
+                    continuar = false;
+                    break;
+                // Ordenar
+                case 1:
+                    System.out.println("\n");
+                    util.ordenarLista(lista);
+                    break;
+
+                // Listar primos
+                case 2:
+                    util.imprimePrimos(lista);
+                    break;
+
+                // Ordenar primos
+                case 3:
+                    util.ordenaPrimos(lista);
+                    break;
+
+                // Gerar nova lista
+                case 4:
+                    lista = util.gerarLista(tamanhoLista, limiteLista);
+                    System.out.println("\n\nNova lista gerada!\n\n");
+                    break;
+            }
+        }
+    }
+}
---
+++ src/Util.java
@@ -0,0 +1,76 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Util {
+
+    public int[] gerarLista(int tamanho, int limite){
+        Random gerador = new Random();
+
+        int[] lista = new int[tamanho];
+
+        for(int i = 0; i < tamanho; i++){
+            lista[i] = gerador.nextInt(limite+1);
+        }
+
+        return lista;
+    }
+
+    public void imprimeLista(int[] lista){
+        for(int n : lista){
+            System.out.println(n);
+        }
+        System.out.println("\n");
+    }
+
+    public void ordenarLista(int[] lista) {
+        int[] listaOrdenada = lista.clone();
+
+        for (int i = 0; i < listaOrdenada.length - 1; i++) {
+            for (int k = 0; k < listaOrdenada.length - 1 - i; k++) {
+
+                if (listaOrdenada[k] > listaOrdenada[k + 1]) {
+                    int temp = listaOrdenada[k];
+                    listaOrdenada[k] = listaOrdenada[k + 1];
+                    listaOrdenada[k + 1] = temp;
+                }
+            }
+        }
+
+        imprimeLista(listaOrdenada);
+    }
+
+    public int[] separaPrimos(int[] lista) {
+        int[] primos = new int[lista.length];
+        int k = 0;
+
+        for (int n : lista) {
+            boolean ehPrimo = true;
+
+            if(n < 2){
+                continue;
+            }
+
+            for (int i = 2; i < n; i++) {
+                if (n % i == 0) {
+                    ehPrimo = false;
+                    break;
+                }
+            }
+
+            if (ehPrimo) {
+                primos[k] = n;
+                k++;
+            }
+        }
+
+        return Arrays.copyOf(primos, k);
+    }
+
+    public void imprimePrimos(int[] lista){
+        imprimeLista(separaPrimos(lista));
+    }
+
+    public void ordenaPrimos(int[] lista){
+        ordenarLista(separaPrimos(lista));
+    }
+}
